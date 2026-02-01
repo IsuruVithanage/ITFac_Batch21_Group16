c2c18312849e898eb2f8fcd5654afb033f1a01bb
Ashan Salinda <ashansalinda5@gmail.com>
2026-02-02 00:06:33 +0530

    TC_ADMIN_PLANT_07

--- cypress/e2e/features/ui/admin_plant.feature
+++ cypress/e2e/features/ui/admin_plant.feature
@@ -9,3 +9,9 @@ Feature: Admin Plant Management
     When the "admin" navigates to the plant list page
     And the admin clicks on the Add Plant button
     Then the admin should be navigated to the Add Plant page
+
+  @TC_ADMIN_PLANT_07 @215104B
+  Scenario: Verify admin can navigate to edit plant page using Edit button
+    When the "admin" navigates to the plant list page
+    And the admin clicks on the edit button for the first plant in the list
+    Then the admin should be navigated to the Edit Plant page
--- cypress/support/step_definitions/ui/plant_steps.js
+++ cypress/support/step_definitions/ui/plant_steps.js
@@ -4,10 +4,29 @@ When("the {string} navigates to the plant list page", (_role) => {
   cy.visit("/ui/plants");
 });
 
+// -----------------------------------
+// Add plant
+// -----------------------------------
 When("the admin clicks on the Add Plant button", () => {
   cy.contains("a", "Add a Plant").should("be.visible").click();
 });
 
 Then("the admin should be navigated to the Add Plant page", () => {
   cy.url().should("include", "/ui/plants/add");
+});
+
+
+// -----------------------------------
+// Edit plant
+// -----------------------------------
+When("the admin clicks on the edit button for the first plant in the list", () => {
+  cy.get("tbody tr")
+    .should("have.length.gt", 0, "Plant table is empty! Cannot click Edit.")
+    .first()
+    .find("a[title='Edit']")
+    .click();
+});
+
+Then("the admin should be navigated to the Edit Plant page", () => {
+  cy.url().should("include", "/ui/plants/edit/");
 });
